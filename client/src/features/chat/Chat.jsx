e9442eb41c26764d9d58e4dbb4728e9cbe297b28
filipslunsky <fslunsky@gmail.com>
2024-12-18 10:43:29 +0100

    added chat list displaying to Chat.jsx

--- client/src/features/chat/Chat.jsx
+++ client/src/features/chat/Chat.jsx
@@ -1,7 +1,50 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { getChats } from "./state/slice";
+
 const Chat = () => {
+    const dispatch = useDispatch();
+    
+    const chats = useSelector(state => state.chats.chats);
+    const user = useSelector(state => state.user.user);
+
+    useEffect(() => {
+        dispatch(getChats(user.email));
+    }, []);
+
+    useEffect(() => {
+        console.log(chats);
+
+    }, []);
+
     return (
         <>
-            <h2>Chat</h2>
+            {
+                chats.map(item => {
+                    return (
+                        <div className="chatItemContainer" key={item.chat_id}>
+                            <p className="chatItemName">
+                                {
+                                    item.chat_name === null
+                                    ?
+                                    'unnamed chat'
+                                    :
+                                    item.chat_name
+                                }
+                            </p>
+                            <div className="chatParticipantsContainer">
+                                {
+                                    item.participants.map((person, index) => {
+                                        return (
+                                            <span className="chatParticipant" key={index}>{`${person.first_name} ${person.last_name}`}</span>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </>
     );
 }
