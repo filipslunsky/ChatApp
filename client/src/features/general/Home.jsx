970743bd52240f8da56c3aca3e11411b3c345e59
filipslunsky <fslunsky@gmail.com>
2025-01-19 17:49:26 +0100

    styling added to Home.jsx

--- client/src/features/general/Home.jsx
+++ client/src/features/general/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './home.css';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -19,9 +20,9 @@ const Home = () => {
         <>  <div className="homeContainer">
                 <h2 className="homeTitle">Welcome to ChutUp</h2>
                 <p className="homeText">If you are new here, you can register and start chatting immediately, it is easy, it is fast and it is free.</p>
-                <Link to={'/user/register'}>Create Account</Link>
+                <Link to={'/user/register'} className="homeLink">Create Account</Link>
                 <p className="homeText">If you already have an account, you can log into it right away.</p>
-                <Link to={'/user/login'}>Login to My Account</Link>
+                <Link to={'/user/login'} className="homeLink">Login to My Account</Link>
             </div>
         </>
     );
